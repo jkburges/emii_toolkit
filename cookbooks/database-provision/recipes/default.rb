302218e77eb972c63ff2cb42a51457de07d4065a
Jon Burgess <jkburges@gmail.com>
2013-09-12 10:07:41 +1000

    Including ruby dependencies.

--- cookbooks/database-provision/recipes/default.rb
+++ cookbooks/database-provision/recipes/default.rb
@@ -1,5 +1,9 @@
+# Database creation requires the pg gem.
+include_recipe "postgresql::ruby"
+
 # create a postgresql database
 postgresql_database 'geoserver' do
   connection ({:host => "localhost", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+  template 'template_postgis'
   action :create
 end
