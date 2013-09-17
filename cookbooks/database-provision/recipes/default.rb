6406198bb183557f1f51dbc3d94cb6feb36cdf7c
Jon Burgess <jkburges@gmail.com>
2013-09-17 20:01:07 +1000

    Provisioning database with liquibase script.

--- cookbooks/database-provision/recipes/default.rb
+++ cookbooks/database-provision/recipes/default.rb
@@ -4,9 +4,19 @@ include_recipe 'liquibase'
 # Database creation requires the pg gem.
 include_recipe "postgresql::ruby"
 
+database_name = 'geoserver'
+
+connection = ({
+                :host => "localhost",
+                :port => 5432,
+                :username => 'postgres',
+                :password => node['postgresql']['password']['postgres'],
+                :database => database_name
+              })
+
 # create a postgresql database
-postgresql_database 'geoserver' do
-  connection ({:host => "localhost", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+postgresql_database database_name do
+  connection connection
   template 'template_postgis'
   action :create
 end
@@ -16,10 +26,25 @@ package 'libpostgresql-jdbc-java'
 
 include_recipe 'git'
 
-# TODO: checkout database git repo
+# Checkout database git repo
+bash "install_something" do
+  user "vagrant"
+  cwd "/home/vagrant"
+  code <<-EOH
+  git clone https://github.com/jkburges/emii_db.git
+  EOH
+  not_if { ::File.exists?('/home/vagrant/emii_db') }
+end
 
-# TODO: apply changelog
-
+# Apply changelog
+liquibase_migrate 'migrate' do
+  change_log_file '/home/vagrant/emii_db/changelog.xml'
+  jar "#{node[:liquibase][:install_path]}/liquibase.jar"
+  connection connection
+  classpath '/usr/share/java/postgresql-jdbc4.jar'
+  driver 'org.postgresql.Driver'
+  adapter :postgresql
+end
 
 # convenience script to generate liquibase changelog.
 # TODO: fork the liquibase cookbook and add other commands.
