d3d5afd18354c345bb7c0cd92256e1a9bcb086e1
Jon Burgess <jkburges@gmail.com>
2013-09-18 16:37:19 +1000

    Now inserting reference to newly generated changelog in to main changelog.

--- bin/diffChangeLog.sh
+++ bin/diffChangeLog.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-vagrant ssh -- "workspace/bin/run_liquibase.sh diffChangeLog > workspace/emii_db/${1}.changelog.xml"
+changelog=${1}.changelog.xml
+
+vagrant ssh -- "workspace/bin/run_liquibase.sh diffChangeLog > workspace/emii_db/${changelog}"
+
+# Insert reference in to main changelog.
+sed -i bak "s/<!-- Do not edit this line. --\>/<include file=\'${changelog}\' relativeToChangelogFile=\'true\' \/\>\\
+    <!-- Do not edit this line. --\>/g" workspace/emii_db/changelog.xml
