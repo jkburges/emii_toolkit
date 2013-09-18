d381256f80cd6251cce610d3c6398ad562dc9697
Jon Burgess <jkburges@gmail.com>
2013-09-18 16:40:59 +1000

    No longer creates a backup file.

--- bin/diffChangeLog.sh
+++ bin/diffChangeLog.sh
@@ -5,5 +5,5 @@ changelog=${1}.changelog.xml
 vagrant ssh -- "workspace/bin/run_liquibase.sh diffChangeLog > workspace/emii_db/${changelog}"
 
 # Insert reference in to main changelog.
-sed -i bak "s/<!-- Do not edit this line. --\>/<include file=\'${changelog}\' relativeToChangelogFile=\'true\' \/\>\\
+sed -i '' "s/<!-- Do not edit this line. --\>/<include file=\'${changelog}\' relativeToChangelogFile=\'true\' \/\>\\
     <!-- Do not edit this line. --\>/g" workspace/emii_db/changelog.xml
